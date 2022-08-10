a3e03f5ecff303619444b0c08e98f59c6fa3db89
Minh Vu <minh.nv.2045@aptechlearning.edu.vn>
2022-08-10 16:57:20 +0700

    MV

--- database/fit4life.sql
+++ database/fit4life.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE fit4life;
 
+USE fit4life;
+
 CREATE TABLE `Counter` (
   `vcid`       int(6) NOT NULL,  
   `visits`     int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `Counter` (`vcid`, `visits`)
-VALUES  (1, 0);
+VALUES  (1, 1232);

09d68e8bf11e943a25b777f145e56d60423a00bd
Minh Vu <minh.nv.2045@aptechlearning.edu.vn>
2022-08-12 13:55:57 +0700

    MV

--- app/controllers/TYController.js
+++ app/controllers/TYController.js
@@ -3,7 +3,7 @@ app.controller('TYController', function ($timeout, $location, $interval, $scope)
     $scope.time = 5;
     $timeout(function () {
         $location.path('/');
-    }, 4000);
+    }, 5000);
     $interval(function () {
         $scope.time = $scope.time - 1;
     }, 1000);
